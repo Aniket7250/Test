6c7d3be9103fd5994207477c6ac04626ed905e36
Aniket7250 <56511829+Aniket7250@users.noreply.github.com>
2021-07-23 15:11:10 +0530

    Delete hp.py

--- hp.py
+++
@@ -1,9 +0,0 @@
-adj = input("adjective: ")
-verb1 = input("verb: ")
-verb2 = input("verb: ")
-famous_person = input("famous person: ")
-
-hp = f"hp laptops are so {adj}! It makes me so excited all the \
-time because I love to {verb1} . Stay hydrated and {verb2} like {famous_person}."
-
-print(hp)
