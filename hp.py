fef412b1ad04cde6463d9b38992bc56e8851fc6f
aniket kumar <aniketkr45@gmail.com>
2021-07-23 14:25:31 +0530

    #1

---
+++ hp.py
@@ -0,0 +1,9 @@
+adj = input("adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person: ")
+
+hp = f"hp laptops are so {adj}! It makes me so excited all the \
+time because I love to {verb1} . Stay hydrated and {verb2} like {famous_person}."
+
+print(hp)
---
+++ zombie.py
@@ -0,0 +1,9 @@
+adj = input("adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person: ")
+
+zombie = f"Zombie are so {adj}! It makes me afraid all the \
+time because I fear  to {verb1} . Stay alive and {verb2} like {famous_person}."
+
+print(zombie)
