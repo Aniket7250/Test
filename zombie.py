fd092a5138d4ac194a0e755db9c673c003aa6a55
Aniket7250 <56511829+Aniket7250@users.noreply.github.com>
2021-07-23 15:11:46 +0530

    Delete zombie.py

--- zombie.py
+++
@@ -1,9 +0,0 @@
-adj = input("adjective: ")
-verb1 = input("verb: ")
-verb2 = input("verb: ")
-famous_person = input("famous person: ")
-
-zombie = f"Zombie are so {adj}! It makes me afraid all the \
-time because I fear  to {verb1} . Stay alive and {verb2} like {famous_person}."
-
-print(zombie)
